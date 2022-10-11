6f0d2219181cad0721274b8f00eda7642268f39c
Nemanja Lazarevic <nemanja.lazarevic.la@gmail.com>
2022-10-11 09:51:56 +0100

    deprecate ViewPropTypes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { StyleSheet, View, ViewPropTypes,I18nManager } from 'react-native'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, View, I18nManager } from "react-native";
+import { ViewPropTypes } from "deprecated-react-native-prop-types";
 
 // compatability for react-native versions < 0.44
 const ViewPropTypesStyle = ViewPropTypes
   ? ViewPropTypes.style
-  : View.propTypes.style
-let direction = I18nManager.isRTL? 'right' : 'left';
-let opDirection = I18nManager.isRTL? 'Left' : 'Right';
+  : View.propTypes.style;
+let direction = I18nManager.isRTL ? "right" : "left";
+let opDirection = I18nManager.isRTL ? "Left" : "Right";
 const styles = StyleSheet.create({
   outerCircle: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   innerCircle: {
-    overflow: 'hidden',
-    justifyContent: 'center',
-    alignItems: 'center',
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
   },
   leftWrap: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     [`${direction}`]: 0,
   },
   halfCircle: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     [`borderTop${opDirection}Radius`]: 0,
     [`borderBottom${opDirection}Radius`]: 0,
   },
-})
+});
 
 function percentToDegrees(percent) {
-  return percent * 3.6
+  return percent * 3.6;
 }
 
 export default class PercentageCircle extends Component {
@@ -46,13 +47,13 @@ export default class PercentageCircle extends Component {
     percent: PropTypes.number.isRequired, // eslint-disable-line react/no-unused-prop-types
     children: PropTypes.node,
     containerStyle: ViewPropTypesStyle,
-    outerCircleStyle: ViewPropTypesStyle
+    outerCircleStyle: ViewPropTypesStyle,
   };
 
   static defaultProps = {
-    color: '#f00',
-    shadowColor: '#999',
-    bgColor: '#e9e9ef',
+    color: "#f00",
+    shadowColor: "#999",
+    bgColor: "#e9e9ef",
     borderWidth: 2,
     children: null,
     containerStyle: null,
@@ -60,17 +61,17 @@ export default class PercentageCircle extends Component {
 
   computeDerivedState() {
     const { props } = this;
-    const percent = Math.max(Math.min(100, props.percent), 0)
-    const needHalfCircle2 = percent > 50
-    let halfCircle1Degree
-    let halfCircle2Degree
+    const percent = Math.max(Math.min(100, props.percent), 0);
+    const needHalfCircle2 = percent > 50;
+    let halfCircle1Degree;
+    let halfCircle2Degree;
     // degrees indicate the 'end' of the half circle, i.e. they span (degree - 180, degree)
     if (needHalfCircle2) {
-      halfCircle1Degree = 180
-      halfCircle2Degree = percentToDegrees(percent)
+      halfCircle1Degree = 180;
+      halfCircle2Degree = percentToDegrees(percent);
     } else {
-      halfCircle1Degree = percentToDegrees(percent)
-      halfCircle2Degree = 0
+      halfCircle1Degree = percentToDegrees(percent);
+      halfCircle2Degree = 0;
     }
 
     return {
@@ -79,16 +80,14 @@ export default class PercentageCircle extends Component {
       halfCircle2Styles: {
         // when the second half circle is not needed, we need it to cover
         // the negative degrees of the first circle
-        backgroundColor: needHalfCircle2
-          ? props.color
-          : props.shadowColor,
+        backgroundColor: needHalfCircle2 ? props.color : props.shadowColor,
       },
-    }
+    };
   }
 
   renderHalfCircle(rotateDegrees, halfCircleStyles) {
-    const { radius, color } = this.props
-    const key = I18nManager.isRTL ? 'right' : 'left';
+    const { radius, color } = this.props;
+    const key = I18nManager.isRTL ? "right" : "left";
     return (
       <View
         style={[
@@ -106,7 +105,7 @@ export default class PercentageCircle extends Component {
               width: radius,
               height: radius * 2,
               borderRadius: radius,
-              overflow: 'hidden',
+              overflow: "hidden",
               transform: [
                 { translateX: radius / 2 },
                 { rotate: `${rotateDegrees}deg` },
@@ -118,11 +117,11 @@ export default class PercentageCircle extends Component {
           ]}
         />
       </View>
-    )
+    );
   }
 
   renderInnerCircle() {
-    const radiusMinusBorder = this.props.radius - this.props.borderWidth
+    const radiusMinusBorder = this.props.radius - this.props.borderWidth;
     return (
       <View
         style={[
@@ -138,15 +137,12 @@ export default class PercentageCircle extends Component {
       >
         {this.props.children}
       </View>
-    )
+    );
   }
 
   render() {
-    const {
-      halfCircle1Degree,
-      halfCircle2Degree,
-      halfCircle2Styles,
-    } = this.computeDerivedState()
+    const { halfCircle1Degree, halfCircle2Degree, halfCircle2Styles } =
+      this.computeDerivedState();
 
     return (
       <View
@@ -165,6 +161,6 @@ export default class PercentageCircle extends Component {
         {this.renderHalfCircle(halfCircle2Degree, halfCircle2Styles)}
         {this.renderInnerCircle()}
       </View>
-    )
+    );
   }
 }
